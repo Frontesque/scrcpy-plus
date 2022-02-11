3766080d84328316e31cce2897e3c1d35c505203
Kenny <27463495+Frontesque@users.noreply.github.com>
2022-02-10 22:53:42 -0500

    Update builder.config.js

--- builder.config.js
+++ builder.config.js
@@ -3,7 +3,7 @@ const ICONS_DIR = 'build/icons/'
 const windowsOS = {
   win: {
     icon: ICONS_DIR + 'win-icon.ico',
-    publisherName: 'michal',
+    publisherName: 'frontesque',
     target: 'nsis'
   },
 
