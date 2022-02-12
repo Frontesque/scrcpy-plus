a1b01578755055b6f2669849f3fd93e7f197c56a
Front <27463495+Frontesque@users.noreply.github.com>
2022-02-12 10:02:10 -0500

    forgot to bump version number

--- src/renderer/nuxt.config.js
+++ src/renderer/nuxt.config.js
@@ -7,7 +7,7 @@
 
 module.exports = {
   env: {
-    version: "1.0.1",
+    version: "1.0.2",
     scrcpyVersion: "1.22",
     adbVersion: "31.0.3-7562133",
     //scrcpyPath: "src/extraResources/scrcpy/", //Development
