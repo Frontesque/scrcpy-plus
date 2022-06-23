ea4c94e0266cc123316fb2e037690fcda36aaf1f
Kenny <27463495+Frontesque@users.noreply.github.com>
2022-06-23 09:31:23 -0400

    docs: :bookmark: bump version

--- src/renderer/nuxt.config.js
+++ src/renderer/nuxt.config.js
@@ -7,7 +7,7 @@
 
 module.exports = {
   env: {
-    version: "1.1.1",
+    version: "1.1.2",
     scrcpyVersion: "1.23",
     adbVersion: "31.0.3-7562133",
     //scrcpyPath: "src/extraResources/scrcpy/", //Development
