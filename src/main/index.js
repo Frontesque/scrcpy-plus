f262ca166d8a1a4102ed84986112a21b21f6751e
Kenny <27463495+Frontesque@users.noreply.github.com>
2022-11-07 06:17:41 -0800

    Possible fix for issue #13

--- src/main/index.js
+++ src/main/index.js
@@ -4,7 +4,7 @@ import { app } from 'electron'
 app.on('window-all-closed', function () {
   // On macOS it is common for applications and their menu bar
   // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') app.quit();
+  //if (process.platform !== 'darwin') app.quit();
 
 
   //---   Kill the ADB server   ---//
