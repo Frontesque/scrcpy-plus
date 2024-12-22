15f0b344898a1ac36d2b131ae2ba611724808ab5
ScarZ <43269183+scarrrr316@users.noreply.github.com>
2024-12-23 06:31:26 +0800

    feat: Implement Wi-Fi pairing via QR code
    - Generate QR code for scanning
    - Start mDNS search for devices
    - Automatically pair device upon discovery

---
+++ src/renderer/components/qrCode.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="mt-7 mb-3" style="height: 300px; width: 300px; margin: 0 auto;" v-html="qrSVG" />
+</template>
+
+<script>
+import { renderSVG } from 'uqr'
+export default {
+    props: {
+        qrText: {
+            type: String,
+        }
+    },
+    data() {
+        return {
+            qrSVG: renderSVG(this.qrText)
+        }
+    }
+}
+</script>
