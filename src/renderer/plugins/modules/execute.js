a1ab42bde3a0b7a8314d1bf0ce42290d2ee5e1b0
Lucian I. Last <li@last.nl>
2025-01-03 01:04:00 +0100

    Add homebrew to exec

--- src/renderer/plugins/modules/execute.js
+++ src/renderer/plugins/modules/execute.js
@@ -2,16 +2,29 @@ const { exec } = require("child_process");
 const os = require('os');
 
 module.exports = (cmd) => {
-    const isWindows = os.type() == "Windows_NT" ? true : false;
+    const isWindows = os.type() == "Windows_NT";
+    const isMac = os.type() == "Darwin";
 
     return new Promise((resolve, reject) => {
-
+        let command = cmd
+        let opts = {};
+        // CD to PreInstalled SCRCPY On Windows
+        if (isWindows) {
+            command = 'cd '+process.env.scrcpyPath+ '&' + command;
+        }
+        if (isMac) {
+            opts["shell"] = "/bin/zsh"
+            opts["env"] = process.env
+            opts["env"]["PATH"] += ":/opt/homebrew/bin"
+            // command = "source ~/.zshrc; " + command;
+        }
         exec(
-          (isWindows ? ('cd '+process.env.scrcpyPath+ '&') : ('')) + cmd, // CD to PreInstalled SCRCPY On Windows
-          (error, stdout, stderr) => {
-            if (error || stderr) reject(error || stderr);
-            resolve(stdout);
-          }
+            command, 
+            opts,
+            (error, stdout, stderr) => {
+                if (error || stderr) reject(error || stderr);
+                resolve(stdout);
+            }
         );
     
     });
